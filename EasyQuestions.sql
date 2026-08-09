e8c705d4d740210f3099996ef174e4f36a6a3881
TANU SINGH <tanujadon5@gmail.com>
2026-08-10 01:29:12 +0530

    Update EasyQuestions.sql

--- EasyQuestions.sql
+++ EasyQuestions.sql
@@ -2,7 +2,7 @@
 SQL Questions
 
 
--- 01. https://datalemur.com/questions/sql-histogram-tweets
+-01. https://datalemur.com/questions/sql-histogram-tweets
 
 with cte as(
 select user_id,count(msg) as cnt
@@ -14,4 +14,12 @@ select cnt as tweet_bucket,count(user_id)
 from cte
 group by tweet_bucket
 order by tweet_bucket
+
+
+-02. https://datalemur.com/questions/matching-skills
+
+SELECT candidate_id FROM candidates
+where skill IN ('Python', 'Tableau', 'PostgreSQL')
+group by candidate_id
+having count(skill) = 3
 
